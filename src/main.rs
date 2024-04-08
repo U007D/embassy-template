0ec789a9de3c0cc47907d8320e91849e6b85355a
Brad Gibson <u007d@humanenginuity.com>
2024-04-08 15:02:51 -0700

    Update main.rs
    remove unused import

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{Level, Output};
-use embassy_time::{Duration, Timer};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
